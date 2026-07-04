d58faa99551c5e32e40416f1fa95aff5ab7bf475
Rafael <rafaellorenzo.cc@gmail.com>
2026-07-03 21:40:19 -0300

    Adicionando testes do módulo de transferências

---
+++ test/transferencias.test.js
@@ -0,0 +1,60 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Transferencias', () =>{
+    describe('POST /transferencias', () => {
+        it('Must return 201 success if transfer is equal or greater than 10', async () => {
+            //Capture Auth token
+            const responseLogin = await request('http://localhost:3000')
+            .post('/login') //Endpoint method called
+            .set('Content-Type','application/json') //Post header
+            .send({ //Body of the Post
+                'username': 'julio.lima',
+                'senha': '123456'
+            })
+            
+            const token = responseLogin.body.token;
+
+            const response = await request('http://localhost:3000')
+            .post('/transferencias')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                contaOrigem: 1,
+                contaDestino: 2,
+                valor: 11,
+                token: ""
+            })
+            
+            expect(response.status).to.equal(201);
+            console.log(response.body);
+        });
+
+        it('Must return 422 fail if transfer is smaller than 10', async () => {
+             //Capture Auth token
+            const responseLogin = await request('http://localhost:3000')
+            .post('/login') //Endpoint method called
+            .set('Content-Type','application/json') //Post header
+            .send({ //Body of the Post
+                'username': 'julio.lima',
+                'senha': '123456'
+            })
+            
+            const token = responseLogin.body.token;
+
+            const response = await request('http://localhost:3000')
+            .post('/transferencias')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                contaOrigem: 1,
+                contaDestino: 2,
+                valor: 9,
+                token: ""
+            })
+            
+            expect(response.status).to.equal(422);
+            console.log(response.body);
+        });
+    })
+})
